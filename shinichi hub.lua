77c38344a8b6b3537f5bc51f508cf26093172c8c
BinbinvoGamingVNN <122621965+BinbinvoGamingVNClone@users.noreply.github.com>
2025-01-31 18:00:10 +0700

    Update shinichi hub.lua

--- shinichi hub.lua
+++ shinichi hub.lua
@@ -1,4 +1,25 @@
-if game.PlaceId == 2753915549 then
+if game:GetService("Players").LocalPlayer.PlayerGui.Main:FindFirstChild("ChooseTeam") then
+repeat task.wait()
+    if game:GetService("Players").LocalPlayer.PlayerGui:WaitForChild("Main").ChooseTeam.Visible == true then
+        if getgenv().Team == "Marines" then
+            for i, v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container["Marines"].Frame.TextButton.Activated)) do
+                for a, b in pairs(getconnections(game:GetService("UserInputService").TouchTapInWorld)) do
+                    b:Fire() 
+                end
+                v.Function()
+            end 
+        else
+            for i, v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam.Container["Pirates"].Frame.TextButton.Activated)) do
+                for a, b in pairs(getconnections(game:GetService("UserInputService").TouchTapInWorld)) do
+                    b:Fire() 
+                end
+                v.Function()
+            end 
+        end
+    end
+until game.Players.LocalPlayer.Team ~= nil and game:IsLoaded()
+end
+elseif game.PlaceId == 2753915549 then
         World1 = true
     elseif game.PlaceId == 4442272183 then
         World2 = true
@@ -11006,14 +11027,6 @@ Mc:Button({
     end
 })
 
-
-Sea Event Đây Nha
-
-if World1 or World2 then
-E:Section("Go To The Third Sea")
-end
-
-if World3 then
 E:Button({
   Name = "Remove Fog",
   Callback = function()
